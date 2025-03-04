838c715205364dc5aa46792763b67c9fe5173456
rowdyGit13 <127695758+rowdyGit13@users.noreply.github.com>
2025-03-04 08:35:51 -0600

    continue with backend setup

---
+++ db/db.ts
@@ -0,0 +1,16 @@
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+//import { notesTable } from "./schema";
+import { profilesTable } from "./schema/profiles-schema";
+
+config({ path: ".env.local" });
+
+const schema = {
+  profiles: profilesTable,
+  //notes: notesTable
+};
+
+const client = postgres(process.env.DATABASE_URL!);
+
+export const db = drizzle(client, { schema });
